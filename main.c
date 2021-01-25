1222e3ebe499e36ca117a64b45efee3e23d8cabe
Johanna Lutter <joh_lu@web.de>
2021-01-25 11:50:37 +0100

    Ausgangsversion ohne Funktionen

--- main.c
+++ main.c
@@ -1,24 +1,85 @@
 /**
- *
- *
- *
- **/
+*   Tag des Jahres berechnen
+*   Autor: Johanna
+*   Erstellt am: 11.01.2020
+**/
+
 
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- *
- *
- **/
-int day_of_the_year(int day, int month, int year)
-{
-
-    return 0;
-}
-
 int main()
 {
-    printf("Tag des Jahres: %i\n", day_of_the_year(31, 12, 2018));
-    return 0;
+    int i;
+    int year = 0;
+    int month = 0;
+    int day = 0;
+    int dayofyear = 0;
+    int leapyear;
+    int days_per_month[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+
+    //Eingabeauforderung des Jahres und löschen des Tastaturpuffers
+    printf("Bitte geben Sie das Jahr ein. ");
+    scanf("%i", &year);
+    fflush(stdin);
+
+    //Prüfung, ob das eingegebene JAhr ein Schaltjahr ist
+    //Jahr ohne Rest durch 4 teilbar?
+    if (year%4 == 0)
+    {
+         // Jahr ohne Rest durch 100 teilbar?
+        if (year%100 == 0)
+        {
+            //Jahr ohne Rest durch 400 teilbar?
+            if(year%400 == 0)
+            {
+                leapyear = 1;
+            }
+            else
+            {
+                leapyear = 0;
+            }
+        }
+        else
+        {
+          leapyear = 1;
+        }
+    }
+
+    //setzten der Tage im Februar auf 29, wenn das eingegebene Jahr ein Schaltjahr ist
+    if (leapyear == 1)
+    {
+        days_per_month[1] = 29;
+    }
+
+    //Eingabeauforderung des Jahres und löschen des Tastaturpuffers
+    printf("\nBitte geben Sie den Monat ein. ");
+    scanf("%i", &month);
+    fflush(stdin);
+
+    //Eingabeauforderung des Jahres und löschen des Tastaturpuffers
+    printf("\nBitte geben Sie den Tag ein. ");
+    scanf("%i", &day);
+    fflush(stdin);
+
+    //Berechnung des Tages im Jahr
+    for (i = 0; i < month - 1; i ++)
+    {
+       dayofyear = dayofyear + days_per_month[i];
+       //printf("%i", days_per_month[i]);
+    }
+
+    //Addieren der angegebenen Tage
+    dayofyear = dayofyear + day;
+
+    //Ausgabe
+    if(dayofyear > 366)
+    {
+        printf("Der Tag des Jahres ist zu gross, es konnte keine Berechnung statfinden!");
+    }
+    else
+    {
+        printf("Das eingegebene Datum ist der %i. Tag des Jahres.", dayofyear);
+    }
 }
